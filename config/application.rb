69e67f1734aef698d8484080b168fb37854f6a57
Evaldas Grudzinskas <prohdo@icloud.com>
2016-01-27 20:43:49 +0200

    Add root route; config rails generators

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,12 @@ module FiscusOpen
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :slim
+      g.test_framework  :rspec, fixture: false
+      g.stylesheets     false
+      g.javascripts     false
+    end
   end
 end
