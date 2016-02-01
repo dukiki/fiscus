41d8e8c21d3eed1347d2ddfdf009b41f50ae4b12
Evaldas Grudzinskas <prohdo@icloud.com>
2016-02-01 21:51:19 +0200

    Add shop model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201194858) do
+ActiveRecord::Schema.define(version: 20160201195054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20160201194858) do
   create_table "incomes", force: :cascade do |t|
     t.string   "name"
     t.decimal  "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shops", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
