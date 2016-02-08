e72461d93066f73a0d050215cf017105b413615e
Evaldas Grudzinskas <prohdo@icloud.com>
2016-02-08 22:10:55 +0200

    Add categories resourcesj

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,7 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  def loggeg_in?
+  def logged_in?
     !current_user.nil?
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+
+  resources :categories
 end
